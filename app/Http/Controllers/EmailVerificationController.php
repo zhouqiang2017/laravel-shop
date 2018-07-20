51c7584b43887ee9121087ae5e28e67bd7c05469
zhouqiang <l>
2018-07-20 11:14:50 +0800

    触发激活邮箱

---
+++ app/Http/Controllers/EmailVerificationController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Exception;
+use Illuminate\Support\Facades\Cache;
+use App\Notifications\EmailVerificationNotification;
+use Mail;
+
+class EmailVerificationController extends Controller
+{
+    public function verify(Request $request)
+    {
+        $email = $request->email;
+        $token = $request->token;
+        if (!$email || !$token) {
+            throw new Exception('验证链接不正确');
+        }
+        if ($token != Cache::get('email_verification_' . $email)) {
+            throw new Exception('验证链接不正确或已过期');
+        }
+        if (!$user = User::where('email', $email)->first()) {
+            throw new Exception('用户不存在');
+        }
+        Cache::forget('email_verification_'.$email);
+
+        $user->update(['email_verified' => true]);
+
+        return view('pages.success', ['msg' => '邮箱验证成功']);
+    }
+    public function send(Request $request)
+    {
+        $user = $request->user();
+        // 判断用户是否已经激活
+        if ($user->email_verified) {
+            throw new Exception('你已经验证过邮箱了');
+        }
+        // 调用 notify() 方法用来发送我们定义好的通知类
+        $user->notify(new EmailVerificationNotification());
+
+        return view('pages.success', ['msg' => '邮件发送成功']);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,13 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['middleware' => 'auth'], function() {
+    Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
+    Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
 
     // 开始
     Route::group(['middleware' => 'email_verified'], function() {
-        Route::get('/test', function() {
-            return 'Your email is verified';
-        });
+      
     });
     // 结束
 });
