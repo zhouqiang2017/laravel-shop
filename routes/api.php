e6db719f5e26a39a94fb814e2fc0d9d74c080531
zhouqiang <l>
2018-08-02 14:47:52 +0800

    初始化后台管理

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::middleware('api')->get('/debcry', function () {
+    dd(bcrypt(123456));
+});
